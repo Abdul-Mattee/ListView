6432374243a5f2d70956db778d5f6c6866fe7cb5
Abdullah Qayyum <bsef19a039@pucit.edu.pk>
2022-05-29 13:01:23 +0500

    adapter added

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listContainer);
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("abdullah");
+        arrayList.add("mattee");
+        arrayList.add("ahmad");
+        arrayList.add("umer");
+        arrayList.add("wahab");
+        arrayList.add("dani");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
     }
 }
